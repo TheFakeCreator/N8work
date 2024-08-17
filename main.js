3002277423eb1ce6d17db244fd376f565471d017
TheFakeCreator <166818795+TheFakeCreator@users.noreply.github.com>
2024-08-17 13:56:09 +0530

    added the code for fetching url

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 import axios from 'axios';
+import { response } from 'express';
+import wbk from'wikibase-sdk';
 
-async function fetchWikipediaArticle(title) {
-    const url = `/api/w/api.php`;
-    const params = {
-        action: 'query',
-        format: 'json',
-        titles: title,
-        prop: 'extracts',
-        explaintext: true,
-    };
-    try {
-        const response = await axios.get(url, { params });
-        const pages = response.data.query.pages;
-        if (!pages) {
-            throw new Error('No pages found in response');
-        }
-        const page = pages[Object.keys(pages)[0]];
-        if (!page || page.missing) {
-            throw new Error('Article not found');
-        }
-        return page.extract;
-    } catch (error) {
-        console.error('Error fetching Wikipedia article:', error);
-    }
-}
+const wdk = wbk({
+    instance: 'https://www.wikidata.org',
+    sparqlEndpoint: 'https://query.wikidata.org/sparql'
+})
 
-fetchWikipediaArticle('Albert_Einstein').then(console.log);
+const url = wdk.searchEntities({
+    search: 'Elon Musk',
+    format: 'json',
+    language: 'en',
+    limit:30,
+})
+
+axios.get(url).then((response) => {
+    const wikidata_response = response.data;
+    const search_items = wikidata_response.search.map((item) => {
+        return {
+            id: item.id,
+            label: item.label,
+            desc: item.description,
+        }
+    })
+    console.log(search_items);
+})
