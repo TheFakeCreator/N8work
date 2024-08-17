c42187b34d601a0eaba7e472b84d9d71148a95e4
TheFakeCreator <166818795+TheFakeCreator@users.noreply.github.com>
2024-08-17 15:12:45 +0530

    added the code to create a html file out of the response

--- main.js
+++ main.js
@@ -1,27 +1,75 @@
 import axios from 'axios';
 import { response } from 'express';
 import wbk from'wikibase-sdk';
+import { JSDOM } from 'jsdom';
+import express from 'express';
+
+
+
+
 
 const wdk = wbk({
     instance: 'https://www.wikidata.org',
     sparqlEndpoint: 'https://query.wikidata.org/sparql'
 })
 
-const url = wdk.searchEntities({
-    search: 'Elon Musk',
-    format: 'json',
-    language: 'en',
-    limit:30,
-})
+const html = `
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite App</title>
+  </head>
+  <body>
+    <div id="app">
+      
+    </div>
+    <script type="module" src="/main.js"></script>
+  </body>
+</html>
 
-axios.get(url).then((response) => {
-    const wikidata_response = response.data;
-    const search_items = wikidata_response.search.map((item) => {
-        return {
-            id: item.id,
-            label: item.label,
-            desc: item.description,
-        }
+`;
+
+const dom = new JSDOM(html);
+const document = dom.window.document;
+const app = document.getElementById('app');
+
+function getSearchQuery(searchQuery,searchQueryLimit){
+    const url = wdk.searchEntities({
+        search: searchQuery,
+        format: 'json',
+        language: 'en',
+        limit: searchQueryLimit,
     })
-    console.log(search_items);
-})
+    
+    axios.get(url).then((response) => {
+        const wikidata_response = response.data;
+        const search_items = wikidata_response.search.map((item) => {
+            return {
+                id: item.id,
+                label: item.label,
+                desc: item.description,
+            }
+        })
+
+        for(let i=0;i<search_items.length;i++){
+            let qid = search_items[i].id;
+            let label = search_items[i].label;
+            let description = search_items[i].desc;
+            const newElement = document.createElement('div');
+            newElement.id = `${qid}`;
+            newElement.className = `${label}`;
+            newElement.textContent = `${description}`;
+
+            app.appendChild(newElement);
+        }
+        console.log(dom.serialize());
+    })
+}
+
+let searchQueryLimit = 30;
+let searchQuery = 'Elon Musk';
+getSearchQuery(searchQuery, searchQueryLimit);
+
